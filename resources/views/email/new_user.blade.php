16e5f12a4314251529a0697bdeb60c50ff31a120
Nahid Bin Azhar <nahid.dns@gmail.com>
2016-04-20 17:35:23 +0600

    fix views

--- resources/views/email/new_user.blade.php
+++ resources/views/email/new_user.blade.php
@@ -1,6 +1,6 @@
 @extends('site.layouts.email')
 @section('contents')
-	<h2 style="margin: 0; padding: 0; font-size: 16px; font-weight: bold; color: #fd2323; text-align: left;">Dear, {{$user['name']}}</h2>
+	<h2 style="margin: 0; padding: 0; font-size: 16px; font-weight: bold; color: #fd2323; text-align: left;">Dear {{$user['name']}},</h2>
 	Thank you for joining with me as an. {{$user['role']}} in <a href="http://nahid.co">nahid.co</a> site. <br/>
 	Login <a href="http://nahid.co/login">here</a> to see your profile.
 	@endsection
