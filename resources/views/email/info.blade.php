34686462af36d34c9420604c82efbc8bb02e54e8
Nahid Bin Azhar <nahid.dns@gmail.com>
2016-04-20 17:39:44 +0600

    fix views

--- resources/views/email/info.blade.php
+++ resources/views/email/info.blade.php
@@ -1,5 +1,5 @@
-@extends('site.layouts.email)
+@extends('site.layouts.email')
 @section('contents')
-	<h2 style="margin: 0; padding: 0; font-size: 16px; font-weight: bold; color: #fd2323; text-align: left;">Dear, {{$user->name}}</h2>
+	<h2 style="margin: 0; padding: 0; font-size: 16px; font-weight: bold; color: #fd2323; text-align: left;">Dear {{$user->name}},</h2>
 	Thank you for contacting with me. Your message is already in queue. I will contact with you as soon as possible.
 	@endsection
--- resources/views/email/new_comment.blade.php
+++ resources/views/email/new_comment.blade.php
@@ -1,6 +1,6 @@
-@extends('site.layouts.email)
+@extends('site.layouts.email')
         @section('contents')
-            <h2 style="margin: 0; padding: 0; font-size: 16px; font-weight: bold; color: #fd2323; text-align: left;">Dear, {{$users['name']}}</h2>
+            <h2 style="margin: 0; padding: 0; font-size: 16px; font-weight: bold; color: #fd2323; text-align: left;">Dear {{$users['name']}},</h2>
             A new comment was published in <a href="{{url('/diary/read/'.$comment['diary_id'])}}">nahid.co</a> where you commented before. <br/>
-            Please checkout now and response his/her comment.
+            Please check it out now and response his/her comment.
         @endsection
